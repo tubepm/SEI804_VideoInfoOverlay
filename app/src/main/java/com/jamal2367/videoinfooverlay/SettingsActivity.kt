026adc9d6fa340b6d8b2789b1682e1378eccd2ca
Jamal Rothfuchs <jamal2362@googlemail.com>
2024-05-09 11:24:25 +0200

    Add about info

--- app/src/main/java/com/jamal2367/videoinfooverlay/SettingsActivity.kt
+++ app/src/main/java/com/jamal2367/videoinfooverlay/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.jamal2367.videoinfooverlay
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
@@ -13,6 +15,20 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.activity_settings, rootKey)
+
+            // set up "App Version" preference
+            val preferenceAppVersion = findPreference<Preference>("pref_version_key")
+            val packageName = requireContext().packageName
+            val packageInfo = requireContext().packageManager.getPackageInfo(packageName, 0)
+            val versionName = packageInfo.versionName
+
+            preferenceAppVersion?.title = "${getString(R.string.app_name)} $versionName"
+            preferenceAppVersion?.summary = "https://github.com/jamal2362/SEI804_VideoInfoOverlay"
+            preferenceAppVersion?.setIcon(R.drawable.ic_info_24dp)
+            preferenceAppVersion?.setOnPreferenceClickListener {
+                Toast.makeText(requireContext(), "❤", Toast.LENGTH_LONG).show()
+                true
+            }
         }
     }
 }
